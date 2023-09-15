455ed7897f7a31cb6450e7efee0b05bdd518e290
KarlaR <karlarobertati@gmail.com>
2023-09-14 21:06:06 -0300

    frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { ReadComponent } from './components/read/read.component';
+import { DeleteComponent } from './components/delete/delete.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReadComponent,
+    DeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
